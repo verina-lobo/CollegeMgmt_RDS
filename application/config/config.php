1ea1aabf36e8350f82e43476b94f4690142ed475
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-06-28 17:17:04 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://smithaapp.azurewebsites.net';
+$config['base_url'] = 'http://52.66.203.119/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://smithaapp.azurewebsites.net';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
