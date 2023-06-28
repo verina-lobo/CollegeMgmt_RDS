303a133a77db515de49017a76a39042ab01edc7b
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-06-28 17:20:27 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://52.66.203.119/CollegeMgmt/';
+$config['base_url'] = 'http://52.66.203.119/CollegeMgmt_RDS/';
 
 /*
 |--------------------------------------------------------------------------
