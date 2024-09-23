d65313f8b169440a619882c855169eda95c0c4f8
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-09-23 19:04:28 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'cmts170124.ccuod208iptk.ap-south-1.rds.amazonaws.com',
+	'hostname' => 'testmig.cxi82go2u7hz.us-east-1.rds.amazonaws.com',
 	'username' => 'admin',
 	'password' => 'admin123',
 	'database' => 'college_mgmt',
