a7698cd1e44ca78aaddb981817ff39539d0c5981
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-09-23 19:18:03 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://smithaapp-f9azb6enasbfg5ct.canadacentral-01.azurewebsites.net';
+$config['base_url'] = 'https://testsmithaapp.azurewebsites.net';
 
 /*
 |--------------------------------------------------------------------------
